78e2ccbe004bef6af2c863d8635796818694d65c
eunah112 <shdmsdk2012@gmail.com>
2024-06-18 18:01:05 +0900

    feat: 디자인

---
+++ js/sidehtml.js
@@ -0,0 +1,10 @@
+function offDisplay() {
+    document.getElementById("offdisplay").style.display = "none";
+    document.getElementById("ondisplay").style.display = "block"
+}
+
+//const
+
+//offDisplay.addEventListener('click', () => {
+  //      offDisplay.style.display = 'block';
+    //})
