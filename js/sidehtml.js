139c65dcf193b4d1deeba880162341055c3e0441
eunah112 <shdmsdk2012@gmail.com>
2024-06-19 17:55:25 +0900

    quizhtml 수정

--- js/sidehtml.js
+++ js/sidehtml.js
@@ -3,8 +3,27 @@ function offDisplay() {
     document.getElementById("ondisplay").style.display = "block"
 }
 
-//const
+//이미지 리스트
+const images = [
+  "/img/drone_mal1.png",
+  "",
+  ""
+];
 
-//offDisplay.addEventListener('click', () => {
-  //      offDisplay.style.display = 'block';
-    //})
+let currentIndex = 0;
+
+// 이미지 표시 엘리먼트
+const imageDisplay = document.getElementById('imageDisplay');
+
+// 다음 이미지 버튼
+const nextImageBtn = document.getElementById('nextImageBtn');
+nextImageBtn.addEventListener('click', () => {
+    // 인덱스 증가, 마지막 이미지면 처음으로 돌아감
+    currentIndex = (currentIndex + 1) % images.length;
+    imageDisplay.src = images[currentIndex];
+});
+
+function offDisplay() {
+    document.getElementById('offdisplay').classList.add('hidden');
+    document.getElementById('ondisplay').classList.remove('hidden');
+}
