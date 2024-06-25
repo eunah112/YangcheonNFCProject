5fdce8c7e4fcab71a4eef3ba079ff6342f1f7c08
eunah112 <shdmsdk2012@gmail.com>
2024-06-25 11:09:17 +0900

    feat: quiz html, js 수정

---
+++ js/RobotQuiz.js
@@ -0,0 +1,64 @@
+const questions = [
+    "1번<br>오늘 수업에서 배운 로봇 중<br>AI 로봇이 있다.",
+    "2번<br>로봇의 눈 역할을 하는 센서는<br>카메라이다.",
+    "3번<br>로봇은 항상 정해진 일을<br>수행한다."
+];
+const answers = [true, true, false];
+let currentQuestion = 0;
+
+const app1Container = document.getElementById('App1Container');
+const app2Container = document.getElementById('App2Container');
+const questionElement = document.getElementById('question');
+const OButton = document.getElementById('OButton');
+const XButton = document.getElementById('XButton');
+const OButtonContainer = document.getElementById('OButtonContainer');
+const XButtonContainer = document.getElementById('XButtonContainer');
+const answer1 = document.getElementById('answer1');
+const answer2 = document.getElementById('answer2');
+const answerfin1 = document.getElementById('answerfin1');
+const answerfin2 = document.getElementById('answerfin2');
+const nextButton = document.getElementById('nextButton');
+
+function checkAnswer(isCorrect) {
+    if (isCorrect) {
+        if (currentQuestion === questions.length - 1) {
+            answerfin1.classList.remove('hidden');
+        } else {
+            answer1.classList.remove('hidden');
+        }
+        answer2.classList.add('hidden');
+        answerfin2.classList.add('hidden');
+    } else {
+        if (currentQuestion === questions.length - 1) {
+            answerfin2.classList.remove('hidden');
+        } else {
+            answer2.classList.remove('hidden');
+        }
+        answer1.classList.add('hidden');
+        answerfin1.classList.add('hidden');
+    }
+    nextButton.classList.remove('hidden');
+}
+
+OButton.addEventListener('click', () => {
+    checkAnswer(answers[currentQuestion] === true);
+});
+
+XButton.addEventListener('click', () => {
+    checkAnswer(answers[currentQuestion] === false);
+});
+
+nextButton.addEventListener('click', () => {
+    currentQuestion++;
+    if (currentQuestion < questions.length) {
+        questionElement.innerHTML = questions[currentQuestion];
+        answer1.classList.add('hidden');
+        answer2.classList.add('hidden');
+        answerfin1.classList.add('hidden');
+        answerfin2.classList.add('hidden');
+        nextButton.classList.add('hidden');
+    } else {
+        app1Container.classList.add('hidden');
+        app2Container.classList.remove('hidden');
+    }
+});
--- js/sidehtml.js
+++ js/sidehtml.js
@@ -2,28 +2,3 @@ function offDisplay() {
     document.getElementById("offdisplay").style.display = "none";
     document.getElementById("ondisplay").style.display = "block"
 }
-
-//이미지 리스트
-const images = [
-  "/img/drone_mal1.png",
-  "",
-  ""
-];
-
-let currentIndex = 0;
-
-// 이미지 표시 엘리먼트
-const imageDisplay = document.getElementById('imageDisplay');
-
-// 다음 이미지 버튼
-const nextImageBtn = document.getElementById('nextImageBtn');
-nextImageBtn.addEventListener('click', () => {
-    // 인덱스 증가, 마지막 이미지면 처음으로 돌아감
-    currentIndex = (currentIndex + 1) % images.length;
-    imageDisplay.src = images[currentIndex];
-});
-
-function offDisplay() {
-    document.getElementById('offdisplay').classList.add('hidden');
-    document.getElementById('ondisplay').classList.remove('hidden');
-}
