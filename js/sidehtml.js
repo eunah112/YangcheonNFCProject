ee3a657c6629ffa9ece1c126af0504dcde863800
eunah112 <shdmsdk2012@gmail.com>
2024-06-26 17:59:41 +0900

    robot 설명 수정

--- js/sidehtml.js
+++ js/sidehtml.js
@@ -4,3 +4,34 @@ function offDisplay() {
     document.getElementById("offdisplay").style.display = "none";
     document.getElementById("ondisplay").style.display = "block"
 }
+
+const images = [
+    { src: "/img/drone_mal1.png", alt: "드론 이미지 1" },
+    { src: "/img/drone_mal2.png", alt: "드론 이미지 2" },
+    { src: "/img/drone_mal3.png", alt: "드론 이미지 3" }
+];
+
+let currentIndex = 0;
+
+function updateImage() {
+    const imageElement = document.querySelector('#content img');
+    imageElement.src = images[currentIndex].src;
+    imageElement.alt = images[currentIndex].alt;
+}
+
+document.getElementById('next').addEventListener('click', function() {
+    if (currentIndex < images.length - 1) {
+        currentIndex++;
+        updateImage();
+    }
+});
+
+document.getElementById('prev').addEventListener('click', function() {
+    if (currentIndex > 0) {
+        currentIndex--;
+        updateImage();
+    }
+});
+
+window.onload = updateImage;
+
