1e9f8556d00aec8984b6dbb1db1c5e3561f48568
eunah112 <shdmsdk2012@gmail.com>
2024-06-26 14:47:13 +0900

    퀴즈 시작페이지

--- js/RobotQuiz.js
+++ js/RobotQuiz.js
@@ -1,13 +1,14 @@
 document.addEventListener("touchstart", function() {}, true);
 
 const questions = [
-    "Q.01<br>오늘 수업에서 배운 로봇 중<br>AI 로봇이 있다.",
-    "Q.02<br>로봇의 눈 역할을 하는 센서는<br>카메라이다.",
-    "Q.03<br>로봇은 항상 정해진 일을<br>수행한다."
+    "오늘 수업에서 배운 로봇 중<br>AI 로봇이 있다.",
+    "로봇의 눈 역할을 하는 센서는<br>카메라이다.",
+    "로봇은 항상 정해진 일을<br>수행한다."
 ];
 const answers = [true, true, false];
 let currentQuestion = 0;
 
+const app0Container = document.getElementById('App0Container');
 const app1Container = document.getElementById('App1Container');
 const app2Container = document.getElementById('App2Container');
 const questionElement = document.getElementById('question');
@@ -20,6 +21,7 @@ const answer2 = document.getElementById('answer2');
 const answerfin1 = document.getElementById('answerfin1');
 const answerfin2 = document.getElementById('answerfin2');
 const nextButton = document.getElementById('nextButton');
+const quizStartButton = document.getElementById('QuizStart');
 
 function checkAnswer(isCorrect) {
     if (isCorrect) {
@@ -63,4 +65,10 @@ nextButton.addEventListener('click', () => {
         app1Container.classList.add('hidden');
         app2Container.classList.remove('hidden');
     }
+});
+
+quizStartButton.addEventListener('click', () => {
+    app0Container.classList.add('hidden');
+    app1Container.classList.remove('hidden');
+    questionElement.innerHTML = questions[currentQuestion];
 });
